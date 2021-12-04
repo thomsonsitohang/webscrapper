f48f81029daf54869bba0ae612ba7bcab0fcf78d
thomsonsitohang <thomson.sitohang@gmail.com>
2021-12-04 11:02:18 +0700

    initialize project

---
+++ src/test/java/com/example/webscrapper/WebscrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebscrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
